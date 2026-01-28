a44f3e77dfd01ad819855d96168d1656c32cb861
Matt Young <matt@mattyoung.us>
2026-01-28 02:50:12 -0600

    Add the ability to use ->local on dates to use local timezone

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+
+        Carbon::macro('local', function () {
+            return $this->tz(config('app.display_timezone', 'UTC'));
+        });
     }
 
     protected function configureDefaults(): void
--- config/app.php
+++ config/app.php
@@ -66,6 +66,8 @@ return [
     */
 
     'timezone' => 'UTC',
+    'display_timezone' => env('APP_DISPLAY_TIMEZONE', 'UTC'),
+
 
     /*
     |--------------------------------------------------------------------------
