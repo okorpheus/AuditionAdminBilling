b1b46190fcc6deb0ae93ab90acd525d7680cab85
Matt Young <matt@mattyoung.us>
2026-01-28 01:44:18 -0600

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::resource('clients', ClientController::class)->names('clients');
+});
 
-Route::view('clients', 'clients.index')
-    ->middleware(['auth', 'verified'])
-    ->name('clients.index');
+// Route::view('dashboard', 'dashboard')
+//    ->middleware(['auth', 'verified'])
+//    ->name('dashboard');
+
+// Route::view('clients', 'clients.index')
+//    ->middleware(['auth', 'verified'])
+//    ->name('clients.index');
 
 require __DIR__.'/settings.php';
