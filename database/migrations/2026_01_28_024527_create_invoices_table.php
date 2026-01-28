6c4deaa298fec9f6291752b264dbc8b26fa40cf8
Matt Young <matt@mattyoung.us>
2026-01-27 20:56:40 -0600

    Initial work on invoice class

---
+++ database/migrations/2026_01_28_024527_create_invoices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_number')->unique();
+            $table->foreignIdFor(Client::class);
+            $table->json('lines');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
