b7a395dfaab7feb2165ee8faebc2195d3f505b9d
Matt Young <matt@mattyoung.us>
2026-01-27 20:29:16 -0600

    Contact model and migraton

---
+++ app/Models/Contact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    public $fillable = ['first_name', 'last_name', 'email', 'phone'];
+}
